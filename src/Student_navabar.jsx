8fd2b1f97d23a7e45076fab58a735dbeed95c756
srivaishnavichintha <srivaishnavichintha@gmail.com>
2025-07-27 18:33:03 +0530

    resolved errors

--- src/Student_navabar.jsx
+++ src/Student_navabar.jsx
@@ -3,9 +3,9 @@ import { useState, useRef, useEffect } from "react";
 import logo from "./assets/react.png";
 import moon from "./assets/moon.png";
 import sun from "./assets/sun.png";
-import env from "./assets/envelope.png"
-import whiteenv from "./assets/white_env.png"
- import { Link } from "react-router-dom";
+import env from "./assets/envelope.png";
+import whiteenv from "./assets/white_env.png";
+import { Link } from "react-router-dom";
 
 export default function Student_navbar() {
   const [isDark, setIsDark] = useState(false);
@@ -25,22 +25,22 @@ export default function Student_navbar() {
   return (
     <nav ref={navRef} id="nava">
       <div className="nav-left">
-        <img src={logo} alt="logo" className="logo"/>
-        <a className="nav-con">Courses</a>
-         <Link to="/practice" className="nav-con">Practice</Link>
-        <a className="nav-con">My Leaderboard</a>
-         <a className="nav-con">Contests</a>
-         <a className="nav-con">peer2peer</a>
+        <img src={logo} alt="logo" className="logo" />
+        <Link to="/courses" className="nav-con">Courses</Link>
+        <Link to="/practice" className="nav-con">Practice</Link>
+        <Link to="/leaderboard" className="nav-con">My Leaderboard</Link>
+        <Link to="/contests" className="nav-con">Contests</Link>
+        <Link to="/peer2peer" className="nav-con">peer2peer</Link>
       </div>
       <div className="nav-right">
-         <img src={isDark ? whiteenv : env} />
-         <div id="main_nav"></div>
+        <img src={isDark ? whiteenv : env} />
+        <div id="main_nav"></div>
         <img
-        src={isDark ? sun : moon}
-        alt="theme toggle"
-        onClick={theme}
-        className="theme"
-      />
+          src={isDark ? sun : moon}
+          alt="theme toggle"
+          onClick={theme}
+          className="theme"
+        />
       </div>
     </nav>
   );
